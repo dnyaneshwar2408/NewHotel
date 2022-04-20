28df1ddeeb15a367f264c8e6acf747d77e561c13
ABC <rrajpatil2048@gmail.com>
2022-04-20 20:57:41 +0530

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.hiltonhotel {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+
+
+    opens com.example.hiltonhotel to javafx.fxml;
+    exports com.example.hiltonhotel;
+}
